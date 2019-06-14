5284d8a31386b69d5ed52554badbfbad228c499b
yuki_m <yakan.meroyan@gmail.com>
2019-06-14 13:11:00 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module App004ChatSpace
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    
+    # 必要ないファイルを生成しないように設定
+    config.gerenators do |g|
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.test_framework  false
+    end
+  end
+end
