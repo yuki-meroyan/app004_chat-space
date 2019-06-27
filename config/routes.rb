d0904efe43f7ee7d163a8b3420bd7bac16e5f350
yuki_m <yakan.meroyan@gmail.com>
2019-06-27 18:44:24 +0900

    Update chat-side view group-name for model
    サイドバーに表示するグループ名をモデルに保存されているデータから取得するように変更しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-
-  Rails.application.routes.draw do
-    devise_for :users
-    root to: 'messages#index'
-    resources :users,   only: [:index, :edit, :update]
-    resources :groups,  only: [:new, :create, :edit, :update]
-    # resources :messages, only: [:index, :create]
+  devise_for :users
+  root to: 'messages#index'
+  resources :users,   only: [:index, :edit, :update]
+  resources :groups,  only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
-
 end
