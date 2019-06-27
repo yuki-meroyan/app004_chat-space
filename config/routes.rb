7d7dd6a8b9d9959c65aa9989bfa490ba91981a55
yuki_m <yakan.meroyan@gmail.com>
2019-06-27 17:45:02 +0900

    Update routes.rb
    ルーティングの設定を追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     devise_for :users
     root to: 'messages#index'
-    resources :users, only: [:edit, :update]
+    resources :users,   only: [:index, :edit, :update]
+    resources :groups,  only: [:new, :create, :edit, :update]
+    # resources :messages, only: [:index, :create]
   end
 
 end
