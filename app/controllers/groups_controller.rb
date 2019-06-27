3d7d8877179a6c777b14b305cfd13b1aa70ee99d
yuki_m <yakan.meroyan@gmail.com>
2019-06-27 19:36:28 +0900

    Update groups_controller.rb
    updateアクション内の記述を変更しました。
    editとupdateアクションを呼び出す前に
    set_groupメソッドを実行するように追加しました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,9 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
+  
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -21,6 +22,11 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   protected
@@ -28,4 +34,9 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, { user_ids: [] } )
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
