b6ddedf7f2295a68cdef285ce5b1fd45122ea58a
yuki_m <yakan.meroyan@gmail.com>
2019-06-27 17:44:29 +0900

    Create groups_controller.rb

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,31 @@
+class GroupsController < ApplicationController
+
+  def index
+  end
+  
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  protected
+
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] } )
+  end
+end
