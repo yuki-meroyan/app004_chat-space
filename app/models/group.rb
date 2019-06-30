b237bc5e0c13a3315b6732602039ae0378a47dcb
yuki_m <yakan.meroyan@gmail.com>
2019-06-30 15:15:16 +0900

    Update group.rb
    最後に投稿されたものが
    メッセージであればメッセージを表示する
    画像であれば「画像が投稿されています」と表示する
    未投稿であれば「まだメッセージはありません」と表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,17 @@
 class Group < ApplicationRecord
 
-  has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
-
+  has_many :messages
+  
   validates :name, presence: true, uniqueness: true
+  def show_last_message
+    if (last_message = messages.last).present?
+      # 三項演算子の書き方
+      # 条件式 ? trueの時の値 : falseの時の値
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
