9edbac30cc633e344d5cbc83bdc2eeff615695a2
yuki_m <yakan.meroyan@gmail.com>
2019-06-30 15:16:54 +0900

    Update message.rb
    記述間違い
    （誤）validation
    （正）validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user   
   belongs_to :group    
   
-  validation :content, presence: true, unless: :image?
+  validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 end
