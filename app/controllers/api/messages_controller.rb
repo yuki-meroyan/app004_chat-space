fccc872a0732db270f6e0900829433b41bb78440
yuki_m <yakan.meroyan@gmail.com>
2019-07-20 16:38:22 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # @group = Group.find(params[:group_id])
-    # @messages = @group.messages.includes(:user).where('id > ?', params[:id]);
     @messages = Message.includes(:user).where('id > ? ', params[:id]).where(group_id: params[:group_id]);
   end
 end
